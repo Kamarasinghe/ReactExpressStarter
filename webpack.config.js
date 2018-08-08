1abeb0ad7d7456831586dedf47f06be386d2cf68
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-08-08 12:48:24 -0700

    Installed webpack-dev-server to watch for changes, css-loader and made it modular

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,29 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: [ // Order matters (right to left), need to resolve CSS files before adding to DOM
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: { // Allows same name of CSS but different styles in different components
+              modules: true, // Enables css modules
+              importLoaders: 1, // How many loaders before css-loader should be applied
+              localIdentName: '[name]_[local]_[hash:base64]', // Name of component, name of class/id, randomly generated hash 
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   },
